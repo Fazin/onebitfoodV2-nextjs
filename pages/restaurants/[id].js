132e975b2feb43833fd84fc9f653c09b17e596df
Fabricio <fazin17@gmail.com>
2021-05-16 20:32:14 -0300

    Usando SSR no projeto

--- pages/restaurants/[id].js
+++ pages/restaurants/[id].js
@@ -1,7 +1,17 @@
-import DetailsRestaurant from '../../components/DetailsRestaurant';
+import DetailsRestaurant from "../../components/DetailsRestaurant";
 
-export default function Restaurant() {
-    return (
-        <DetailsRestaurant />
-    )
+export default function Restaurant({ restaurant, isError = false }) {
+  return <DetailsRestaurant restaurant={restaurant} isError={isError} />;
+}
+
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  try {
+    const res = await fetch(`${process.env.apiUrl}/api/restaurants/${id}`);
+    const restaurant = await res.json();
+    const isError = res.ok ? false : true;
+    return { props: { restaurant, isError: isError } };
+  } catch {
+    return { props: { isError: true } };
+  }
 }
